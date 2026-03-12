aa4b8f82eb328d09590a550a31aabb1b2cd8f247
Gambit-03 <g63987087@gmail.com>
2026-03-12 21:22:23 +0530

    Delete backend directory

--- backend/routes/recommend.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
-const { recommendInternships } = require("../controllers/recommendController");
-
-router.post("/", protect, recommendInternships);
-
-module.exports = router;
