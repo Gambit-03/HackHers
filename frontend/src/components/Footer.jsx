c795d6d8e714040f37767526fe80af0b31697258
Gambit-03 <g63987087@gmail.com>
2026-03-12 21:22:37 +0530

    Delete frontend directory

--- frontend/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-primary text-white text-center py-2 mt-4">
-      © {new Date().getFullYear()} PM Internship Scheme | Government of India
-    </footer>
-  );
-}
-export default Footer;
--- frontend/src/components/SHeader.jsx
+++
@@ -1,7 +0,0 @@
-export default function SHeader() {
-  return (
-    <div className="bg-blue-600 text-white text-center p-4 text-xl font-bold">
-      PM Internship Scheme
-    </div>
-  );
-}
